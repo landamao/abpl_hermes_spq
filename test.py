378487b992acd9c0313d1c75f979254ac2f27110
landamao <204676209@qq.com>
2026-05-17 16:39:31 +0800

    chore: 清空仓库，准备重新上传

--- test.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# 获取 main.py 所在目录的绝对路径
-script_dir = os.path.dirname(os.path.abspath(__file__))
-# 向上两级到达 data 目录
-data_dir = os.path.dirname(os.path.dirname(script_dir))
-# 等价写法：data_dir = os.path.abspath(os.path.join(script_dir, '../..'))
-
-print(data_dir)
